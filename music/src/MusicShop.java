d7d44b159969010d4126fc13f6ac3bf479fb5ee5
malushVlad <vlad.malish@gmail.com>
2015-12-21 17:40:55 +0200

    first commit

---
+++ music/src/MusicShop.java
@@ -0,0 +1,6 @@
+/**
+ * Created by vlad on 21.12.15.
+ */
+public class MusicShop {
+    public MusicalInstrument musicalInstrument1  ;
+}
---
+++ music/src/Trumpet.java
@@ -0,0 +1,5 @@
+/**
+ * Created by vlad on 21.12.15.
+ */
+public class Trumpet extends MusicalInstrument {
+}
